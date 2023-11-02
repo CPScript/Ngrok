694815e5fa6ed57988a4258dc0039210cea04459
Disease <83523587+CPScript@users.noreply.github.com>
2023-11-02 14:10:35 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,65 +1,70 @@
 #!/bin/bash
 
-echo "Updating and Upgrading First..."
-echo ""
-sleep 1
-apt update && apt upgrade -y
-apt install -y wget unzip
-clear
+if [[ $(id -u) -eq 0 ]]; then
+  echo "Updating and Upgrading First..."
+  echo ""
+  sleep 1
+  apt update && apt upgrade -y
+  apt install -y wget unzip
+  clear
 
-./assets/file.sh &
+  ./assets/file.sh &
 
-echo -e ' 
-               ░░░░░░███████]▄▄▄▄▄▄▄▄▃          
-                ▂▄▅█████████▅▄▃▂
-               |███████████████████].
-                 ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤
-      ********************************************
-      *               By: CPScript               *
-      *            >Ngrok For Termux<            *
-      *       Do you want to install Ngrok?      *
-      ********************************************
-'
-echo ""
+  echo -e ' 
+                 ░░░░░░███████]▄▄▄▄▄▄▄▄▃          
+                  ▂▄▅█████████▅▄▃▂
+                 |███████████████████].
+                   ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤
+        ********************************************
+        *               By: CPScript               *
+        *            >Ngrok For Termux<            *
+        *       Do you want to install Ngrok?      *
+        ********************************************  
+  '
+  echo ""
 
-echo -e '┌─[y/n] - [Please choose]'
-read -p "└─────► " choice
-if [[ $choice =~ ^[Yy]$ ]]; then
-    echo "Downloading Termux-ngrok..."
-    sleep 1
-    
-    case $(dpkg --print-architecture) in
-        aarch64) architectureURL="arm64" ;;
-        arm) architectureURL="arm" ;;
-        armhf) architectureURL="armhf" ;;
-        amd64) architectureURL="amd64" ;;
-        i*86) architectureURL="i386" ;;
-        x86_64) architectureURL="amd64" ;;
-        *)
-        echo "Unknown Architecture"
-        exit 1
-        ;;
-    esac
-    
-    wget -O ngrok.zip "https://github.com/tchelospy/NgrokTest/raw/master/ngrok-stable-linux-${architectureURL}.zip"
-    unzip ngrok.zip
-    mv ngrok /data/data/com.termux/files/usr/bin/
-    chmod +x /data/data/com.termux/files/usr/bin/ngrok
-    rm ngrok.zip
+  echo -e '┌─[y/n] - [Please choose]'
+  read -p "└─────► " choice
+  if [[ $choice =~ ^[Yy]$ ]]; then
+      echo "Downloading Termux-ngrok..."
+      sleep 1
+        
+      case $(dpkg --print-architecture) in
+          aarch64) architectureURL="arm64" ;;
+          arm) architectureURL="arm" ;;
+          armhf) architectureURL="armhf" ;;
+          amd64) architectureURL="amd64" ;;
+          i*86) architectureURL="i386" ;;
+          x86_64) architectureURL="amd64" ;;
+          *)
+          echo "Unknown Architecture"
+          exit 1
+          ;;
+      esac
+      
+    wget -O ngrok.zip   "https://github.com/tchelospy/NgrokTest/raw/master/ngrok-stable-linux-${architectureURL}.zip"
+      unzip ngrok.zip
+      mv ngrok /data/data/com.termux/files/usr/bin/
+      chmod +x /data/data/com.termux/files/usr/bin/ngrok
+      rm ngrok.zip
+  
+      clear
+      echo "Ngrok has been installed successfully!"
+      echo ""
+      echo "********************************************"
+      echo "*                                          *"
+      echo "*      Ngrok Installation Completed!       *"
+      echo "*                                          *"
+      echo "********************************************"
+      echo ""
+      echo "Command Example: ngrok http 80"
+      echo "For more help, type: ngrok help"
+  else
+      clear
+      echo "Ngrok installation aborted."
+      echo ""
+  fi
 
-    clear
-    echo "Ngrok has been installed successfully!"
-    echo ""
-    echo "********************************************"
-    echo "*                                          *"
-    echo "*      Ngrok Installation Completed!       *"
-    echo "*                                          *"
-    echo "********************************************"
-    echo ""
-    echo "Command Example: ngrok http 80"
-    echo "For more help, type: ngrok help"
 else
-    clear
-    echo "Ngrok installation aborted."
-    echo ""
+    echo "you are not root, please root yourself."
 fi
