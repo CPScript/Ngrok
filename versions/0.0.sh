ee55afb275eb9b20318c74e6f834fbbae13c2a9e
Disease <83523587+CPScript@users.noreply.github.com>
2024-01-08 01:08:56 -0500

    Create 0.0.sh

---
+++ versions/0.0.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo -e ' 
+                   
+                  ░░░░░░███████]▄▄▄▄▄▄▄▄▃          
+                   ▂▄▅█████████▅▄▃▂
+                  |███████████████████].
+                  ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤
+        ********************************************
+        *               By: CPScript               *
+        *           >Easy Ngrok installer<         *
+        *       Do you want to install Ngrok?      *
+        ********************************************  
+  '
+  echo ""
+
+  echo -e '┌─[y/n] - [Please choose]'
+  read -p "└─────► " choice
+  if [[ $choice =~ ^[Yy]$ ]]; then
+      echo "Downloading Ngrok!"
+      echo "Please wait..."
+      sleep 1
+        
+      case $(dpkg --print-architecture) in
+          aarch64) architectureURL="arm64" ;;
+          arm) architectureURL="arm" ;;
+          armhf) architectureURL="armhf" ;;
+          amd64) architectureURL="amd64" ;;
+          i*86) architectureURL="i386" ;;
+          x86_64) architectureURL="amd64" ;;
+          *)
+          echo "Unknown Architecture"
+          exit 1
+          ;;
+      esac
+      
+      wget -O ngrok.zip "https://github.com/tchelospy/NgrokTest/raw/master/ngrok-stable-linux-${architectureURL}.zip"
+      unzip ngrok.zip
+      mv ngrok /data/data/com.termux/files/usr/bin/
+      chmod +x /data/data/com.termux/files/usr/bin/ngrok
+      rm ngrok.zip
+  
+      clear
+      echo "********************************************"
+      echo "*                                          *"
+      echo "*      Ngrok Installation Completed!       *"
+      echo "*     -------------------------------      *"
+      echo "*       Use Example: ngrok http 8080         *"
+      echo "*     For more help, type: ngrok help      *"
+      echo "*                                          *"
+      echo "********************************************"
+  else
+      clear
+      echo "Ngrok installation aborted."
+      sleep 10
+      clear
+  fi
+
+else
+    echo "You're not rooted, please root yourself."
+    echo "How to root yourself: sudo su <password might be required>"
+fi
